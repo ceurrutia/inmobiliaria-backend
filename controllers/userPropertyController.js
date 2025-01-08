41d5a6933ffc86db5ca37e601ad5346da90c9050
Cecilia urrutia <urrutia.cici@gmail.com>
2025-01-08 19:35:33 -0300

    nuevos cambios en controllers

--- controllers/userPropertyController.js
+++ controllers/userPropertyController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const UserProperty = require('../models/userProperty');
+const UserProperty = require('../models/UserProperty');
 
 exports.getAllUsers = async (req, res) => {
   try {
